1889cde3d1aa8f33dc2a17472788c1fd6e7c7e03
Pat Patterson <ppatterson@salesforce.com>
2014-03-25 14:48:56 -0700

    Visualforce on Chromecast... as a service!

--- NodeCast/app.js
+++ NodeCast/app.js
@@ -19,8 +19,8 @@ app.configure(function(){
   app.use(express.urlencoded());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'super mega secret string goes here!' }));
-  app.use(org.expressOAuth({onSuccess: '/', onError: '/oauth/error'}));
+  app.use(express.session({ secret: process.env.CLIENT_SECRET }));
+  app.use(org.expressOAuth({onSuccess: '/castdemosender.html', onError: '/oauth/error'}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -34,10 +34,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
-app.get('/', function(req, res){
+app.get('/castdemosender.html', function(req, res){
   if (req.session.oauth) {
-    var query = 'SELECT Name FROM ApexPage ORDER BY Name';
+    var query = 'SELECT Id, Name, ControllerType, ControllerKey FROM ApexPage ORDER BY Name';
     org.query({query: query, oauth: req.session.oauth}, function(err, resp) {
       if(!err) {
         res.render('castdemosender', {
@@ -45,7 +44,11 @@ app.get('/', function(req, res){
           pages: resp.records
         });
       } else {
-        res.send(err.message);
+        if (err.errorCode === 'INVALID_SESSION_ID') {
+          res.redirect(org.getAuthUri());
+        } else {
+          res.send(err.message);
+        }
       }
     });
   } else {
@@ -53,6 +56,39 @@ app.get('/', function(req, res){
   }
 });
 
-var httpPort = 3000;
+app.get('/records', function(req, res){
+  var objType = req.query.objType;
+
+  // Need to find the name field, since sobjects
+  // like Case don't use 'Name'
+  org.getDescribe({
+    oauth: req.session.oauth,
+    type: objType
+  },function(err, resp){
+    resp.fields.forEach(function(field, i){
+      if (field.nameField) {
+        // Get the first 10 records
+        var query = 'SELECT Id, '+field.name+
+        ' FROM '+objType+
+        ' ORDER BY '+field.name+' LIMIT 10';
+        org.query({
+          query: query, 
+          oauth: req.session.oauth
+        }, function(err, resp) {
+          if(!err) {
+            resp.nameField = field.name.toLowerCase();
+            res.send(resp);
+          } else {
+            console.log(err);
+            res.send(err.message);
+          }
+        });
+        return false;       
+      }
+    });
+  });
+});
+
+var httpPort = Number(process.env.PORT || 3000);
 app.listen(httpPort);
 console.log("Express server listening on port %d in %s mode", httpPort, app.settings.env);
